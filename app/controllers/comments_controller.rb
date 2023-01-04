b42aa957afb4b51e429eec206dc662576a52c3e2
Jerry Owusu <jerryowusu6133@gmail.com>
2023-01-04 18:20:50 +0000

    Update the show view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+  def create 
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article), 
+    notice: 'Your Comment was successfully added'
+  end 
+
+  private 
+
+  def comment_params 
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
+
